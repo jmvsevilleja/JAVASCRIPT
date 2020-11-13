e5e596d2562bc403324b5cd08b7ea945b154f0b3
Jess <jmark@fisicoinc.com>
2020-11-13 23:25:54 +0800

    Added spread, indexof

--- JAVASCRIPT/scripts/data-structure.js
+++ JAVASCRIPT/scripts/data-structure.js
@@ -14,7 +14,16 @@ let popArray = [1, 2, 3];
 console.log('Array remove pop() - last index:', popArray.pop(), popArray);
 
 let spliceArray = [1, 2, 3, 4]
-console.log('Array remove splice(first index, last index, replace) - last removed:', spliceArray.splice(1, 2, 'x'), spliceArray);
+console.log('Array remove/replace splice(start index, count, replace) - last removed:', spliceArray.splice(1, 2, 'x', 'y', 'z'), spliceArray);
+
+let sliceArray = [1, 2, 3, 4]
+console.log('Array copy/extract slice(start index, stop index, replace) - extract:', sliceArray.slice(1, 3), sliceArray);
+
+let thisArray = [true, true, undefined, false, null];
+let thatArray = [1, ...thisArray, 2];
+console.log('Spread Operator:', thisArray, thatArray);
+
+console.log('Index Of:', thatArray.indexOf(false), thatArray.indexOf('false'));
 
 console.log('objects');
 
