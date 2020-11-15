c5f7d7e33ce1e7548c9fb339b4b9f898ff7bdbc4
Jess <jmvsevilleja@gmail.com>
2020-11-15 22:11:30 +0800

    Added object

--- JAVASCRIPT/scripts/data-structure.js
+++ JAVASCRIPT/scripts/data-structure.js
@@ -2,7 +2,7 @@ console.log('--Data Types--');
 console.log('string, number, booleans, undefined, null');
 
 console.log('--Data Structure--');
-console.log('Arrays');
+console.log('--Arrays--');
 let unshiftArray = [1, 2, 3];
 console.log('Array add unshift() - first index 0:', unshiftArray.unshift(0), unshiftArray);
 let pushArray = [1, 2, 3];
@@ -23,7 +23,37 @@ let thisArray = [true, true, undefined, false, null];
 let thatArray = [1, ...thisArray, 2];
 console.log('Spread Operator:', thisArray, thatArray);
 
-console.log('Index Of:', thatArray.indexOf(false), thatArray.indexOf('false'));
+console.log('Index Of false: (-1 if not found)', false, thatArray.indexOf(false), '"false"', thatArray.indexOf('false'));
 
-console.log('objects');
+console.log('--Iterate Array--');
+let newArray = [];
+thatArray.forEach(function (item, index) {
+    if (index % 2 == 0)
+        newArray.push(item);
+});
+console.log('forEach:', newArray);
 
+
+console.log('--Objects--');
+
+const tekkenCharacter = {
+    player: 'Hwoarang',
+    'fighting style': 'Tae Kwon Doe',
+    human: true
+};
+console.log('Access via Object.dot notation:', tekkenCharacter.player);
+console.log('Access via Object["bracket"]:', tekkenCharacter['fighting style']);
+console.log('Delete Object.property:', delete tekkenCharacter.human, tekkenCharacter);
+console.log('Object.hasOwnProperty("player"):', tekkenCharacter.hasOwnProperty('player'));
+console.log('"player" in Object:', 'player' in tekkenCharacter);
+
+
+console.log('--Iterate Object--');
+let newPlayer = [];
+for (let property in tekkenCharacter) {
+    if (property == 'player') {
+        newPlayer.push(tekkenCharacter[property]);
+    }
+};
+console.log('for(let property in Object):', newPlayer);
+console.log('Object keys:', Object.keys(tekkenCharacter));
