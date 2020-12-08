d7044d76529c25121654c0a1e11c82980641e20e
Jess <jmark@fisicoinc.com>
2020-12-08 09:53:43 +0800

    Alternative way to get values from object

--- JAVASCRIPT/scripts/intermediate-algo.js
+++ JAVASCRIPT/scripts/intermediate-algo.js
@@ -1,7 +1,9 @@
 //Remove all elements from an array with the same argument values
 function destroyer(arr) {
     // return arr.filter(num => !Object.values(arguments).slice(1).some(arg => arg == num))
+    // let args = Array.from(arguments).slice(1);
     let args = Object.values(arguments).slice(1);
+
     return arr.filter(num => !args.includes(num));
 }
 console.log('Remove 2,3 from [1, 2, 3, 1, 2, 3]: ', destroyer([1, 2, 3, 1, 2, 3], 2, 3));
