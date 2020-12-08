ffaeaa075c3d76ad04c9f756cab4585f966faf33
Jess <jmark@fisicoinc.com>
2020-12-08 09:52:19 +0800

    Used includes in destroyer challenge

--- JAVASCRIPT/scripts/intermediate-algo.js
+++ JAVASCRIPT/scripts/intermediate-algo.js
@@ -1,5 +1,7 @@
-//Intermediate Algorithm Scripting: Seek and Destroy
+//Remove all elements from an array with the same argument values
 function destroyer(arr) {
-    return arr.filter(num => !Object.values(arguments).slice(1).some(arg => arg == num))
+    // return arr.filter(num => !Object.values(arguments).slice(1).some(arg => arg == num))
+    let args = Object.values(arguments).slice(1);
+    return arr.filter(num => !args.includes(num));
 }
-console.log('Remove all elements from an array with the same argument values.', destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+console.log('Remove 2,3 from [1, 2, 3, 1, 2, 3]: ', destroyer([1, 2, 3, 1, 2, 3], 2, 3));
