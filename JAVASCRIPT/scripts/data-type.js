ae761908febdee01598aebc400ecd74169e25dfc
Jess <jmvsevilleja@gmail.com>
2020-12-28 23:30:33 +0800

    Added a word replace + array of numbers

--- JAVASCRIPT/scripts/data-type.js
+++ JAVASCRIPT/scripts/data-type.js
@@ -5,7 +5,14 @@ console.log('--String--');
 
 let phrase = 'The quick brown fox jumps over the lazy dog';
 console.log('Char At 0', phrase.charAt(0), phrase[0]);
-
-console.log('Replace String', phrase.replace('fox', 'cat'));
+// replace string
+console.log('Replace fox with cat: ', phrase.replace('fox', 'cat'));
+// replace string with regmatch
+console.log('Replace the last word with cat: ', phrase.replace(/([\w]+)$/, 'cat'));
+// replace is not destructive
 
 console.log('String to Array', phrase.split(' '));
+
+console.log('--Array--');
+num = 5;
+console.log('Array of numbers: ', Array.from({ length: num }).map((_, i) => i))
