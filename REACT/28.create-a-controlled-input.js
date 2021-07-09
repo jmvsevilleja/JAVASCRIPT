a6ea301700d4c253eb4126b6a65f28455c4d8096
Jess <jmvsevilleja@gmail.com>
2021-07-09 20:13:32 +0800

    Added controlled form

--- REACT/28.create-a-controlled-input.js
+++ REACT/28.create-a-controlled-input.js
@@ -22,6 +22,7 @@ class ControlledInput extends React.Component {
         { /* Change code below this line */}
         { /* Input property in the local state, and rendered as the value in the input box on the page.
         The component state is the single source of truth regarding the input data. */}
+        {/* Controlled component */}
         <input onChange={this.handleChange} value={this.state.input} />
         { /* Change code above this line */}
         <h4>Controlled Input:</h4>
