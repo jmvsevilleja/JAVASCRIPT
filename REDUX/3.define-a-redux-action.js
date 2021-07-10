6c04b82630996f65a6020b6acd090b99e702ee64
Jess <jmvsevilleja@gmail.com>
2021-07-11 07:58:57 +0800

    Added action definition and dispatch

---
+++ REDUX/3.define-a-redux-action.js
@@ -0,0 +1,6 @@
+// Define an action here:
+const action = {
+  type: "LOGIN"
+}
+
+//Think of Redux actions as messengers that deliver information about events happening in your app to the Redux store.
---
+++ REDUX/4.define-an-action-creator.js
@@ -0,0 +1,9 @@
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+function actionCreator() {
+  return action
+}
+//action creators create objects that represent action events.
+
